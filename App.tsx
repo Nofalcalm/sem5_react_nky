f9a6dde2332b2a3f52af2709fc05cce22d35759a
nofalcalm <novalcalm432@gmail.com>
2023-11-12 16:33:29 +0700

    project awal

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Selamat Datang di bengkel kami</Text>
+    </View>
+  );
+}
+
+export default App;
