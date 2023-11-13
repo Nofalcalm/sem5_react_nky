641453d36a21c97c76901283998f6f3cfbb734f4
yusril091 <yusrilarjuna@gmail.com>
2023-11-13 06:21:27 -0800

    next tame

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import { View, Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreens from './screens/HomeScreens'
+import DetailsScreens from './screens/DetailsScreens'
+import PaymentScreens from './screens/PaymentScreens'
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return (
-    <View>
-      <Text>Selamat Datang di bengkel kami</Text>
-    </View>
-  );
-}
+    return (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Selamat Datang Di Bengkel Kami" component={DetailsScreens} options={{animation:'slide_from_bottom'}}></Stack.Screen>
+        </Stack.Navigator>
+     </NavigationContainer>
+    );  
+};
 
 export default App;
+
+const style =StyleSheet.create({});
---
+++ screens/DetailsScreens.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DetailsScreens = () => {
+    return (
+        <View>
+            <Text>suku cadang motor</Text>
+            <Text>harga suku cadang motor</Text>
+            <Text>menerima service motor</Text>
+        </View>
+    );
+};
+
+const style =StyleSheet.create({});
+
+export default DetailsScreens;
